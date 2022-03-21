d0d2e985a64d553f274b379fcef1e40323898be5
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-21 16:23:29 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,16 +1,14 @@
-import { ajax } from 'rxjs/ajax';
+import { ajax, AjaxResponse } from 'rxjs/ajax';
 
 const apiKey = "d0475be3a1967b1b49dfc02c8128001a";
 const URL =
   "https://api.openweathermap.org/data/2.5/weather?APPID=" +
   apiKey +
   "&units=metric&q=";
-
 var city="Pisa";
 
-const temp = ajax(URL + city); //Costruisco l'observable
-
-const subscribe = temp.subscribe( // Collego un subscriber
-  res => document.getElementById("output").innerHTML=res.response.main.temp,
-  err => console.error(err)
-);
+const obs = ajax(URL + city);
+const subscribe = obs.subscribe({
+  next: ( res: AjaxResponse ) => document.getElementById("output").innerHTML=res.response.main.temp,
+  error: ( err: Error ) => console.error(err)
+});
