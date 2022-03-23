8dcd7ed2cfd9218af63bcffec89bfa602072bba7
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-23 10:58:18 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { Observable } from "rxjs";
-import { ajax, AjaxResponse, AjaxError } from 'rxjs/ajax';
+import { ajax, AjaxResponse, AjaxRequest, AjaxError } from 'rxjs/ajax';
 
 const apiKey : string = "d0475be3a1967b1b49dfc02c8128001a";
 const URL: string =
@@ -7,11 +7,12 @@ const URL: string =
   apiKey +
   "&units=metric&q=";
 var city: string ="Pisa";
-
-const obs: Observable<AjaxResponse> = ajax({
+const request: AjaxRequest = {
   url: URL + city,
   crossDomain: true
-});
+}
+
+const obs: Observable<AjaxResponse> = ajax(request);
 obs.subscribe({
   next: ( res: AjaxResponse ) => document.getElementById("output").innerHTML=res.response.main.temp,
   error: ( err: AjaxError ) => console.error(err.message)
