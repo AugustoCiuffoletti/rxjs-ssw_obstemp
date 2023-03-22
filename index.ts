06e88cd16b2c5bf9e56918f76aefd0ffea8e6f95
AugustoCiuffoletti <augusto.ciuffoletti@unipi.it>
2023-03-22 10:15:51 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,19 +1,18 @@
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 import { ajax, AjaxResponse, AjaxRequest, AjaxError } from 'rxjs/ajax';
-
-const apiKey : string = "d0475be3a1967b1b49dfc02c8128001a";
+const apiKey: string = 'd0475be3a1967b1b49dfc02c8128001a';
 const URL: string =
-  "https://api.openweathermap.org/data/2.5/weather?APPID=" +
+  'https://api.openweathermap.org/data/2.5/weather?APPID=' +
   apiKey +
-  "&units=metric&q=";
-var city: string ="Pisa";
+  '&units=metric&q=';
+var city: string = 'Pisa';
 const request: AjaxRequest = {
   url: URL + city,
-  crossDomain: true
-}
-
+  crossDomain: true,
+};
 const obs: Observable<AjaxResponse> = ajax(request);
 obs.subscribe({
-  next: ( res: AjaxResponse ) => document.getElementById("output").innerHTML=res.response.main.temp,
-  error: ( err: AjaxError ) => console.error(err.message)
+  next: (res: AjaxResponse) =>
+    (document.getElementById('output').innerHTML = res.response.main.temp),
+  error: (err: AjaxError) => console.error(err.message),
 });
